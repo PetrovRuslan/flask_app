8de1bd8b14b578eb701fb564deb445a6d3545f46
Petrov <r.petrov@systtech.ru>
2020-11-27 08:31:55 +0200

    fix reddirect when var None

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ username = os.environ.get('LOGIN')
 
 password = os.environ.get('PASSWORD')
 
-if username == '' or password == '':
+if username == None or password == None:
     @app.route('/')
     def none_var():
         return ("не заданы переменные окружения")
@@ -29,61 +29,57 @@ else:
         username: generate_password_hash(password)
     }
 
-# users = {
-#     username: generate_password_hash(password)
-# }
-
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
-        return username
+    @auth.verify_password
+    def verify_password(username, password):
+        if username in users and \
+                check_password_hash(users.get(username), password):
+            return username
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
-@auth.login_required
-def hello():
-    flash('hello')
-    list_of_files = []
-    for filename in os.listdir(UPLOAD_FOLDER):
-        list_of_files.append(filename)
-    return render_template("index.html",
-                           list_of_files=list_of_files,
-                           UPLOAD_FOLDER=UPLOAD_FOLDER)
+    @app.route('/')
+    @auth.login_required
+    def hello():
+        flash('hello')
+        list_of_files = []
+        for filename in os.listdir(UPLOAD_FOLDER):
+            list_of_files.append(filename)
+        return render_template("index.html",
+                            list_of_files=list_of_files,
+                            UPLOAD_FOLDER=UPLOAD_FOLDER)
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-@auth.login_required
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            print(file.filename)
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file',
-                                    filename=filename))
-        else:
-            flash('not allowed file')
-            return redirect(request.url)
-    return render_template("form.html")
+    @app.route('/upload', methods=['GET', 'POST'])
+    @auth.login_required
+    def upload_file():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                print(file.filename)
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('upload_file',
+                                        filename=filename))
+            else:
+                flash('not allowed file')
+                return redirect(request.url)
+        return render_template("form.html")
 
 
-@app.route('/files/<path:filename>', methods=['GET', 'POST'])
-@auth.login_required
-def download(filename):
-    return send_from_directory(directory=UPLOAD_FOLDER, filename=filename)
+    @app.route('/files/<path:filename>', methods=['GET', 'POST'])
+    @auth.login_required
+    def download(filename):
+        return send_from_directory(directory=UPLOAD_FOLDER, filename=filename)
